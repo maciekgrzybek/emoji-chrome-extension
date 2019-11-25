23198466fb0ffb4601fa8dc82277c17a6a64cf5c
Maciej Grzybek <maciej.grzybek@skybettingandgaming.com>
2019-11-25 11:05:25 +0000

    Initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="h-app w-app">
+      <header className="">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      width: {
+        app: '500px'
+      },
+      height: {
+        app: '700px'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
